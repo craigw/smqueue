7189bcabf552dfd83ef8b019de9390df4d2c3cdb
Craig R Webster <craig@barkingiguana.com>
2010-08-28 15:53:14 +0100

    Clean up the tests.

--- spec/smqueue/adapter/mongrel2_adapter_spec.rb
+++ spec/smqueue/adapter/mongrel2_adapter_spec.rb
@@ -6,41 +6,37 @@ describe SMQueue::Adapter::Mongrel2Adapter do
   it_should_behave_like "an adapter class"
 
   describe "instance" do
-    let(:adapter_instance) { SMQueue::Adapter::Mongrel2Adapter.new }
+    let(:adapter_instance) {
+      protocol = stub_everything('Protocol')
+      SMQueue::Adapter::Mongrel2Adapter.new :protocol => protocol
+    }
     it_should_behave_like "an adapter instance"
 
-    describe "accessing an invalid channel" do
-      before(:each) do
-        protocol = stub_everything('Protocol')
-        channel_class = SMQueue::Adapter::Mongrel2Adapter::Mongrel2RequestChannel
-        channel_class.stubs(:new).returns(:protocol => protocol)
-        @m2 = SMQueue::Adapter::Mongrel2Adapter.new
-      end
+    it "allows setting the protocol implementation" do
+      protocol = stub_everything('Other Protocol Implementation')
+      channel_class = SMQueue::Adapter::Mongrel2Adapter::Mongrel2RequestChannel
+      channel_class.expects(:new).once.with(:protocol => protocol)
+      m2 = SMQueue::Adapter::Mongrel2Adapter.new :protocol => protocol
+      m2.channel 'request'
 
+      channel_class = SMQueue::Adapter::Mongrel2Adapter::Mongrel2ResponseChannel
+      channel_class.expects(:new).once.with(:protocol => protocol)
+      m2 = SMQueue::Adapter::Mongrel2Adapter.new :protocol => protocol
+      m2.channel 'response'
+    end
+
+    describe "accessing an invalid channel" do
       it "should freak out with a nice error message" do
         message = "The channel_name must be 'request' or 'response'."
-        lambda { @m2.channel 'ducks' }.should raise_exception message
+        lambda {
+          adapter_instance.channel 'ducks'
+        }.should raise_exception message
       end
     end
 
     describe "accessing the request channel" do
-      let(:channel) {
-        protocol = stub_everything('Protocol')
-        channel_class = SMQueue::Adapter::Mongrel2Adapter::Mongrel2RequestChannel
-        instance = channel_class.new(:protocol => protocol)
-        channel_class.stubs(:new).returns(instance)
-        m2 = SMQueue::Adapter::Mongrel2Adapter.new
-        m2.channel 'request'
-      }
+      let(:channel) { adapter_instance.channel 'request' }
       it_should_behave_like "an adapter channel"
-
-      it "allows setting the protocol implementation" do
-        protocol = stub_everything('Other Protocol Implementation')
-        channel_class = SMQueue::Adapter::Mongrel2Adapter::Mongrel2RequestChannel
-        channel_class.expects(:new).once.with(:protocol => protocol)
-        m2 = SMQueue::Adapter::Mongrel2Adapter.new :protocol => protocol
-        m2.channel 'request'
-      end
 
       it "freaks out when you try to put a message" do
         message = "You can't put messages on the request channel"
@@ -49,23 +45,8 @@ describe SMQueue::Adapter::Mongrel2Adapter do
     end
 
     describe "accessing the response channel" do
-      let(:channel) {
-        protocol = stub_everything('Protocol')
-        channel_class = SMQueue::Adapter::Mongrel2Adapter::Mongrel2ResponseChannel
-        instance = channel_class.new(:protocol => protocol)
-        channel_class.stubs(:new).returns(instance)
-        m2 = SMQueue::Adapter::Mongrel2Adapter.new
-        m2.channel 'response'
-      }
+      let(:channel) { adapter_instance.channel 'response' }
       it_should_behave_like "an adapter channel"
-
-      it "allows setting the protocol implementation" do
-        protocol = stub_everything('Other Protocol Implementation')
-        channel_class = SMQueue::Adapter::Mongrel2Adapter::Mongrel2ResponseChannel
-        channel_class.expects(:new).once.with(:protocol => protocol)
-        m2 = SMQueue::Adapter::Mongrel2Adapter.new :protocol => protocol
-        m2.channel 'response'
-      end
 
       it "freaks out when you try to get a message" do
         message = "You can't get messages from the response channel"
